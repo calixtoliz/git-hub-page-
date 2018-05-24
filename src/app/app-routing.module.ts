e626b9c41f821936f2bad33d1ae43d115fa4baff
VIRGINIA LIZBETH CASTILLO CALIXTO <calixtolizmex@gmail.com>
2018-05-24 12:12:04 +0200

    primer angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';    
+import { AboutComponent } from './about/about.component';
+
+ 
+import { LizComponent } from './liz/liz.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about/:id',
+    component: AboutComponent
+  },
+
+  {
+    path: 'liz',
+    component: LizComponent
+  }
+];
+
+
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
